0f754b476d30f2207d2ed5e64389d928a12b69e4
Sebastian Fürle <sebastian@footballaddicts.com>
2020-01-15 22:09:03 +0100

    Updates

---
+++ precache-manifest.d24d187cb5e2b20ae037894d906c776a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f69641621545b5becdfdf869641a337",
+    "url": "/toc-tic/index.html"
+  },
+  {
+    "revision": "ace9cddabea32f1e4d6b",
+    "url": "/toc-tic/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "2f2dbbcf43fc209c9597",
+    "url": "/toc-tic/static/js/2.66eb49d4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/toc-tic/static/js/2.66eb49d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ace9cddabea32f1e4d6b",
+    "url": "/toc-tic/static/js/main.4167c42b.chunk.js"
+  },
+  {
+    "revision": "78ed80201b4e10a9a91c",
+    "url": "/toc-tic/static/js/runtime-main.6bfd3efe.js"
+  }
+]);
